6e11f81bc22c35ec1d678dee51f90a4432477924
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2023-04-23 16:41:16 -0300

    home

--- src/main/java/br/com/suprasys/plus/model/Vendedor.java
+++ src/main/java/br/com/suprasys/plus/model/Vendedor.java
@@ -1,5 +1,33 @@
 package br.com.suprasys.plus.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Vendedor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String endereco;
+	private String bairro;
+	private String cidade;
+	private String cep;
+	private String uf;
+	private String email;
 
 }
